325eeb69a54b05e998a69fe643f34ca1ccd1acc1
CHANDAN KUMAR <81066619+ChandanKumar27@users.noreply.github.com>
2025-04-07 14:53:46 +0530

    Add files via upload
    This is a prime number code.

---
+++ Prime.java
@@ -0,0 +1,21 @@
+public class Prime {
+    public static void main(String[] args) {
+        int n = 4;
+        boolean flag = false;
+        if (n==0 || n == 1){
+            flag = true;
+        }
+        
+        for(int i=2;i<=n/2;++i){
+        if(n%i == 0){
+            flag = true;
+            break;
+        }
+        }
+        if (!flag)
+        System.out.println(n + " is a prime number.");
+        else
+        System.out.println(n + "is not a prime number.");
+        
+    }
+}
